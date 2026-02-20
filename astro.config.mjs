59291f9b8e005bcc2849c279c8bad9c42496807a
Aleix Ripoll <aleix.ripoll@gmail.com>
2026-02-20 23:28:33 +0100

    Update Astro configuration with site URL and base path; modify GitHub Actions workflow to use updated action version

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
+  site: "https://aleixripoll.github.io",
+  base: "/pixelperson/",
   integrations: [
     tailwind({ applyBaseStyles: false }),
   ],
